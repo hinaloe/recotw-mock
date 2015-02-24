beae387ca7db5bbfd4b2a329480037594654df56
hina <hina@hinaloe.net>
2015-02-24 16:45:01 +0900

    進捗はなかった.

--- inc.php
+++ inc.php
@@ -4,12 +4,14 @@ class RecoTwPlaceholder
 {
   static public function get_tweet_all ()
   {
+    $ins = new self;
     
   }
   
   static public function user_tweet ()
   {
-    
+    $ins = new self;
+
   }
   
   static public function record_tweet ()
@@ -19,10 +21,17 @@ class RecoTwPlaceholder
   
   static public function count_tweet ()
   {
-    
+    $ins = new self;
+
   }
   
   static public function not_found ()
+  {
+    $ins = new self;
+
+  }
+  
+  private __construct ()
   {
     
   }
