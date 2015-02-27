96ddfb50611e97b3d0735a3abae61d2bad3385fc
hina <hina@hinaloe.net>
2015-02-27 17:03:55 +0900

    some methods

--- inc.php
+++ inc.php
@@ -35,5 +35,37 @@ class RecoTwPlaceholder
   {
     
   }
+
+  private function is_post ()
+  {
+    if ( $_SERVER["REQUEST_METHOD"] === 'POST' )
+      return true;
+    return false;
+  }
+
+  private function get_query_item ($name, $default = null)
+  {
+    if( isset( $_GET[$name]) )
+      return $_GET[$name];
+    return $default;
+  }
+  
+  private function get_post_item ($name, $default = null)
+  {
+    if( isset( $_POST[$name] ))
+      return $_POST[$name];
+    return $default;
+  }
+
+  private function is_ssl ()
+  {
+    if(isset($_SERVER['HTTPS']) && $_SERVER['HTTPS']==='on')
+      return true;
+    // if your server isn't set ENV[HTTPS], you must override this method!
+    // 
+    return false;
+  }
+    
+
   
 }
