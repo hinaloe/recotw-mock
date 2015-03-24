23c449453f3d9076141d5f4b8f0b007664f32003
hinaloe <hina@hinaloe.net>
2015-03-25 02:39:09 +0900

    query

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,48 @@
 <?php
 if(!defined('TANA3NISGAY'))
 {
-  http_response_code(403);
+  http_response_code( 403 );
   echo 'Forbidden';
+}
+
+class RecoTwEntritesModel
+{
+
+  private  $db;
+
+  private  $db_path;
+  private  $name = 'tweets';
+
+  function __construct ()
+  {
+    $db_path = TANA3NISGAY . '/../data/tweets.db';
+
+
+    $this-> db = new SQLite3( $db_path );
+    // migrate
+    $this-> exec ( sprintf ( 'CREATE TABLE IF NOT EXISTS %s (id INTEGER PRIMARY KEY,tweet_id TEXT,content TEXT,target_id TEXT,target_sn TEXT,record_date TEXT)' , $this->name ) );
+  }
+
+  function close ()
+  {
+    $this->db->close();
+  }
+
+  private function query_user_tweet ($screen_name)
+  {
+    $sql = sprintf( 'SELECT * FROM %s where target_sn = :name' ,$this->name);
+    $stmt = $this->db-> prepare($sql);
+    $stmt-> bindValue(':name',$screen_name);
+    $result = $stmt -> execute();
+    return $result->fetchArray();
+  }
+
+  private function query_tweet_count ()
+  {
+    $sql = sprintf( 'SELECT COUNT (*) from %s' , $this->name );
+    $stmt = $this->db -> prepare($sql);
+    $result = $stmt-> execute();
+    return $result->fetchArray(SQLITE3_ASSOC)['COUNT (*)'];
+
+  }
 }
